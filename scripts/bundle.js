c63d26e3567fd409dcf5f679819b4895af297bb3
Danya Grachov <dgrachov@icloud.com>
2024-09-30 09:11:53 +0200

    Add benchmarking tool

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,5 +1,6 @@
 const esbuild = require("esbuild");
 const esbuildPluginTsc = require("esbuild-plugin-tsc");
+const fs = require("fs");
 
 esbuild.build({
     entryPoints: ["./src/bundle.ts"],
@@ -10,5 +11,7 @@ esbuild.build({
     minifyWhitespace: true,
     plugins: [esbuildPluginTsc()]
 });
+
+fs.copyFileSync("./src/benchmark.js", "./dist/benchmark.js");
 
 // Remember that if we change what we build, we should also update .npmignore
---
+++ src/benchmark.js
@@ -0,0 +1,55 @@
+function time(label, fn) {
+    const start = Date.now();
+
+    fn();
+
+    console.log(label, "took", Date.now() - start, "ms");
+}
+
+const stores = [];
+const apps = [];
+const TARGET_STORES = 100;
+const TARGET_APPS = 100;
+
+time(`Benchmark`, () => {
+    time(`Initialize ${TARGET_APPS} app elements`, () => {
+        for (let i = 0; i < TARGET_APPS; i++) {
+            const div = document.createElement("div");
+            div.id = "app-" + i;
+            div.innerHTML = "{{ count }}";
+            document.body.appendChild(div);
+        }
+    });
+    
+    time(`Create ${TARGET_STORES} stores`, () => {
+        for (let i = 0; i < TARGET_STORES; i++) {
+            const store = new WaveStore();
+
+            if (i == 0)
+                store.data.count = 128;
+
+            stores.push(store);
+        }
+    });
+    
+    time(`Create ${TARGET_APPS} apps`, () => {
+        for (let i = 0; i < TARGET_APPS; i++) {
+            const app = new WaveApp("#app-" + i);
+    
+            for (let j = 0; j < stores.length; j++)
+                app.useStore(stores[j]);
+    
+            apps.push(app);
+        }
+    });
+    
+    time(`Mount ${TARGET_APPS} apps`, () => {
+        for (let i = 0; i < apps.length; i++)
+            apps[i].mount();
+    });
+    
+    time(`Unmount ${TARGET_APPS} apps`, () => {
+        for (let i = 0; i < apps.length; i++)
+            apps[i].unmount();
+    });
+});
